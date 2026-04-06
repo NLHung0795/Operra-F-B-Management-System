58e7792d720d1fc43264109fbd5b38d2eb6f61be
NLHung0795 <Hung.NL235942@sis.hust.edu.vn>
2026-04-06 17:58:49 +0700

    config

---
+++ be/operra/src/main/java/com/example/operra/OperraApplication.java
@@ -0,0 +1,13 @@
+package com.example.operra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperraApplication.class, args);
+	}
+
+}
---
+++ be/operra/src/test/java/com/example/operra/OperraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.operra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
