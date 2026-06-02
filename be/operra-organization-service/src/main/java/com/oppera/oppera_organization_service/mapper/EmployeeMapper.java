c99cd8d0a466d24b2b3b3e6c12121b35be12b364
Nguyễn Lê Hưng <Hung.NL235942@sis.hust.edu.vn>
2026-06-02 15:05:45 +0700

    feat(org): add employee mapper

---
+++ be/operra-organization-service/src/main/java/com/oppera/oppera_organization_service/mapper/EmployeeMapper.java
@@ -0,0 +1,25 @@
+package com.oppera.oppera_organization_service.mapper;
+
+import com.oppera.oppera_organization_service.dto.request.EmployeeRequest;
+import com.oppera.oppera_organization_service.dto.request.EmployeeUpdateRequest;
+import com.oppera.oppera_organization_service.dto.response.EmployeeResponse;
+import com.oppera.oppera_organization_service.entity.Employee;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
+public interface EmployeeMapper {
+    Employee toEmployee(EmployeeRequest request);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "branch", ignore = true)
+    @Mapping(target = "department", ignore = true)
+    @Mapping(target = "position", ignore = true)
+    void updateEmployee(@MappingTarget Employee employee, EmployeeUpdateRequest request);
+
+    @Mapping(target = "branchName", source = "branch.name")
+    @Mapping(target = "departmentName", source = "department.name")
+    @Mapping(target = "positionName", source = "position.name")
+    EmployeeResponse toEmployeeResponse(Employee employee);
+}
