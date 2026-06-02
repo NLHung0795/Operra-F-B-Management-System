2d7f14cac3bd2af417c8933bd80529b0411f5edc
Nguyễn Lê Hưng <Hung.NL235942@sis.hust.edu.vn>
2026-06-02 15:56:55 +0700

    feat(org): add scheduling and attendance service with PostgreSQL support

---
+++ be/operra-scheduling-and-attendance-service/src/main/java/com/operra/scheduling_and_attendance_service/OperraSchedulingAndAttendanceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.operra.scheduling_and_attendance_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperraSchedulingAndAttendanceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperraSchedulingAndAttendanceServiceApplication.class, args);
+	}
+
+}
---
+++ be/operra-scheduling-and-attendance-service/src/test/java/com/operra/scheduling_and_attendance_service/SchedulingAndAttendanceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.operra.scheduling_and_attendance_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingAndAttendanceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
