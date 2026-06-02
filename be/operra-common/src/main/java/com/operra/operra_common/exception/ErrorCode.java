2146cfbda4ec6cb4ad8fd8b206d327a209e25ac7
Nguyễn Lê Hưng <Hung.NL235942@sis.hust.edu.vn>
2026-06-02 15:00:57 +0700

    feat(common): extend error codes for account and employee management

--- be/operra-common/src/main/java/com/operra/operra_common/exception/ErrorCode.java
+++ be/operra-common/src/main/java/com/operra/operra_common/exception/ErrorCode.java
@@ -1,5 +1,6 @@
 package com.operra.operra_common.exception;
 
+import lombok.Data;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 
@@ -8,10 +9,13 @@ public enum ErrorCode {
     UNAUTHORIZED(1002,"do not have permission", HttpStatus.FORBIDDEN),
     USER_EXISTED(1001, "user existed", HttpStatus.BAD_REQUEST),
     UNAUTHENTICATED(1003,"UNAUTHENTICATED", HttpStatus.UNAUTHORIZED),
+    USER_NOT_EXISTED(1004,"user not existed", HttpStatus.BAD_REQUEST),
+    USER_ACCOUNT_ERROR(1005,"user account error", HttpStatus.BAD_REQUEST),
     COMPANY_NOT_FOUND(2001, "company not found", HttpStatus.NOT_FOUND),
     BRANCH_NOT_FOUND(2002, "branch not found", HttpStatus.NOT_FOUND),
     DEPARTMENT_NOT_FOUND(2003, "department not found", HttpStatus.NOT_FOUND),
     POSITION_NOT_FOUND(2004, "position not found", HttpStatus.NOT_FOUND),
+    EMPLOYEE_NOT_FOUND(2005, "employee not found", HttpStatus.NOT_FOUND),
     ;
 
     private int code;
