958649dd648b909fa15f6da6aa3adc81cbbed828
Nguyễn Lê Hưng <Hung.NL235942@sis.hust.edu.vn>
2026-06-04 16:05:03 +0700

    feat: update scheduling service urls and kafka configuration

--- be/operra-organization-service/src/main/java/com/oppera/oppera_organization_service/OpperaOrganizationServiceApplication.java
+++ be/operra-organization-service/src/main/java/com/oppera/oppera_organization_service/OpperaOrganizationServiceApplication.java
@@ -3,9 +3,11 @@ package com.oppera.oppera_organization_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableKafka
 public class OpperaOrganizationServiceApplication {
 
 	public static void main(String[] args) {
