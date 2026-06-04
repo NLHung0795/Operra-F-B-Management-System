cdf386fe2be5979e9aaed83f994793168fb8e273
Nguyễn Lê Hưng <Hung.NL235942@sis.hust.edu.vn>
2026-06-04 16:04:46 +0700

    feat: configure jwt security for scheduling service

---
+++ be/operra-scheduling-and-attendance-service/src/main/java/com/operra/scheduling_and_attendance_service/configuration/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,37 @@
+package com.operra.scheduling_and_attendance_service.configuration;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.operra.operra_common.dto.ApiResponse;
+import com.operra.operra_common.exception.ErrorCode;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response /* giup tra ve noi dung mong muon*/ , AuthenticationException authException)
+            throws IOException, ServletException {
+        ErrorCode errorCode = ErrorCode.UNAUTHORIZED;
+
+        response.setStatus(errorCode.getStatusCode().value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        ApiResponse<?> apiRespone = ApiResponse.builder()
+                .code(errorCode.getCode())
+                .message(errorCode.getMessage())
+                .build();
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        response.getWriter().write(mapper.writeValueAsString(apiRespone));
+        response.flushBuffer();
+    }
+}
